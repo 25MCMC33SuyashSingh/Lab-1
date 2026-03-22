12ff5ca3d37eff10c728a50eb276d2c18d1f0612
25MCMC33SuyashSingh <25mcmc33@uohyd.ac.in>
2026-03-22 19:00:16 +0530

    Add files via upload

---
+++ Task-13/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Weather from "./components/weather";
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>🌤 Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
